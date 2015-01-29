39820128360bc61d512fc77ae168ff18f50fd6a1
KarnageKnight <karnageknight@KarnageightsMBP.local.lan>
2015-01-29 19:24:42 +0530

    Initial Commit

---
+++ selection_sort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  selection_sort
+//
+//  Created by KarnageKnight on 29/01/15.
+//  Copyright (c) 2015 KKCPP. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
